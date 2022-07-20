eff97099a9c2515298a51642a0bcad420622f2ac
Vivek John <vivekanand.john@cognizant.com>
2022-07-20 16:47:34 +0530

    Adding tabviewcontroller and podfile

---
+++ ICareOnline/ICareOnline/View/Controllers/MenuController/ICONavigationController.swift
@@ -0,0 +1,57 @@
+//
+//  ICONavigationController.swift
+//  SwiftSideMenu
+//
+//  Created by Evgeny Nazarov on 30.09.14.
+//  Copyright (c) 2014 Evgeny Nazarov. All rights reserved.
+//
+
+import UIKit
+
+class ICONavigationController: ENSideMenuNavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Create a table view controller
+        let tableViewController = ICOMenuTableViewController()
+        
+        // Create side menu
+        sideMenu = ENSideMenu(sourceView: view, menuViewController: tableViewController, menuPosition:.left)
+        
+        // Set a delegate
+        sideMenu?.delegate = self
+        
+        // Configure side menu
+        sideMenu?.menuWidth = 180.0
+        
+        // Show navigation bar above side menu  
+        view.bringSubviewToFront(navigationBar)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ICONavigationController: ENSideMenuDelegate {
+    func sideMenuWillOpen() {
+        print("sideMenuWillOpen")
+    }
+    
+    func sideMenuWillClose() {
+        print("sideMenuWillClose")
+    }
+    
+    func sideMenuDidClose() {
+        print("sideMenuDidClose")
+    }
+    
+    func sideMenuDidOpen() {
+        print("sideMenuDidOpen")
+    }
+    
+    func sideMenuShouldOpenSideMenu() -> Bool {
+        return true
+    }
+}
